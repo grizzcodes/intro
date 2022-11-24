add9f5ba34036d21472b1418e1dccf04b29c722c
Grizz <117692072+grizzcodes@users.noreply.github.com>
2022-11-24 12:59:26 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+def greet():
+	name = input("Enter your name:");
+	print (f"hi {name}");
+#important to include in the brackets the whole print that I want to say
+#{this part is just to include any input from the user}
+
+greet()
